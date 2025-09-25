6e23cfabc94df4deaa34bfa3e69f0743e707b9f5
Sam Shapiro <sam@samshap.com>
2025-09-25 10:24:44 -0700

    cors: allow all vercel.app subdomains via allow_origin_regex

--- backend/app.py
+++ backend/app.py
@@ -38,6 +38,7 @@ if custom_domain:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
